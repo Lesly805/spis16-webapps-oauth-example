17b82c75176b8682974947abf3a932dd52f5e6e6
Lesly805 <77691413+Lesly805@users.noreply.github.com>
2021-06-01 17:32:50 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -15,8 +15,8 @@ app = Flask(__name__)
 
 
 def main():
-    #connection_string = os.environ["MONGO_CONNECTION_STRING"]
-    #db_name = os.environ["MONGO_DBNAME"]
+    connection_string = os.environ["MONGO_CONNECTION_STRING"]
+    db_name = os.environ["MONGO_DBNAME"]
 
     client = pymongo.MongoClient(connection_string)
     db = client[db_name]
